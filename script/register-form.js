ae0ef19b83c862adf55409e0f39b63ff460babf0
Lucas Dordolo <lucasd92@hotmail.com>
2020-11-02 18:22:55 -0300

    All errors in a single alert

--- script/register-form.js
+++ script/register-form.js
@@ -1,6 +1,5 @@
 // Get form by ID
 var form = document.getElementById("register-form");
-
 // Array with form fields
 var inputList = [{id:'name',type: 'text', name:'Name', onError:'At least 6 characters. Ex: John Doe', pattern: /^([a-z]{2,}[\s]+)+([a-z]{2,})$/i},
                  {id:'email',type: 'email', name:'Email', onError:'Invalid email', pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/},
@@ -13,12 +12,9 @@ var inputList = [{id:'name',type: 'text', name:'Name', onError:'At least 6 chara
                  {id:'zipcode',type: 'number', name:'Zip Code', onError:'Must have at least 3 digits', pattern: /^[0-9]{3,}$/},
                  {id:'idn',type: 'number', name:'ID Number', onError:'Must have 7 or 8 digits', pattern: /^[0-9]{7,8}?$/}];
 
-
 // On load add each field with its event listeners
 window.onload = function() {
     //Generate form
-    var submitDiv = form.innerHTML;
-    form.innerHTML = '';
     for(var i=0; i < inputList.length; i++){
         form.innerHTML += '<div>\
                                 <label id="'+ inputList[i].id +'-label">'+ inputList[i].name +'</label>         \
@@ -26,7 +22,10 @@ window.onload = function() {
                                 <p id="'+ inputList[i].id +'-error">'+ inputList[i].onError +'</p>              \
                            </div>';
     }
-    form.innerHTML += submitDiv;
+    //Generate submit button
+    form.innerHTML +=   '<div id="submit-container">        \
+                            <input value="Submit" type="submit"></input>   \
+                        </div>';
     //Add events for form fields
     for(var i=0; i < inputList.length; i++){
         var input = document.getElementById(inputList[i].id +'-input');
@@ -40,24 +39,22 @@ window.onload = function() {
     var nameInput =  document.getElementById('name-input');
     nameInput.addEventListener('keyup',titleTransform);
 };
-
 // Validations
-function genericFocus(){
-    clearError(event.target.id);
+function genericFocus(e){
+    console.log(e.target.id);
+    clearError(e.target.id);
 };
-
 // Generic onBlur function
-function genericBlur(){
+function genericBlur(e){
     //Get first part of the id
-    var listId = event.target.id.split('-')[0];
+    var listId = e.target.id.split('-')[0];
     // Find index in the InputList array
     var index = inputList.findIndex(element => element.id === listId);
     // Validate by id and pattern
-    if(!validateInput(event.target.id, inputList[index].pattern)){
-        showError(event.target.id); 
+    if(!validateInput(e.target.id, inputList[index].pattern)){
+        showError(e.target.id); 
     }
 };
-
 function validateInput(element_id, patt){
     var content = document.getElementById(element_id).value;
     return patt.test(content);
@@ -66,30 +63,31 @@ function showError(itemId){
     var errorMsg = document.getElementById(itemId).nextElementSibling;
     errorMsg.style.display = 'block'; 
 }; 
-
 function clearError(itemId){
     var errorMsg = document.getElementById(itemId).nextElementSibling;
     errorMsg.style.display = 'none';  
 }; 
-
-function submitForm(){
-    event.preventDefault();
+function submitForm(e){
+    e.preventDefault();
+    var inputMsg = '';
     var alertMsg = '';
-
     for(var i=0; i < inputList.length; i++){
         if(!validateInput(inputList[i].id+'-input', inputList[i].pattern)){
             showError(inputList[i].id+'-input'); 
-            alertMsg = 'Error in ' +inputList[i].name + ' field: \n' + inputList[i].onError + '\n';
-            break;
+            alertMsg += 'Error in ' +inputList[i].name + ' field: \n' + inputList[i].onError + '\n';
         }
         var item = document.getElementById(inputList[i].id+'-input');
         if(item.type != 'password'){
-            alertMsg += inputList[i].name + ': ' + item.value + '\n';
+            inputMsg += inputList[i].name + ': ' + item.value + '\n';
         } 
     }
-    alert(alertMsg);
+    if(alertMsg.length > 5){
+        alert(alertMsg);
+    }
+    else{
+        alert(inputMsg);
+    }
 }
-
-function titleTransform(){
-    document.getElementById('form-title').innerText = 'Hello ' +  event.target.value;
+function titleTransform(e){
+    document.getElementById('form-title').innerText = 'Hello ' +  e.target.value;
 };
